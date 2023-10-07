f671c4a434bb73d9c96e7f0a9ea19b54f7ab2120
shade-sicsr <shadedev25@gmail.com>
2023-10-07 19:09:49 +0530

    Completion of Basic Implementations

--- log-reg/Controllers/LoginController.cs
+++ log-reg/Controllers/LoginController.cs
@@ -18,7 +18,16 @@ namespace log_reg.Controllers
             var user = _context.UsersObjects.FirstOrDefault(u => u.Username == username && u.Password == password);
             if (user != null)
             {
-                return RedirectToAction("DisplayUser", "Home");
+
+                var user_id = user.Id;
+                var name = user.Username;
+                var viewModel = new UsersViewModel
+                {
+                    Id = user_id,
+                    Username = name
+                };
+
+                return View("~/Views/Home/DisplayUser.cshtml", viewModel);
             }
             return RedirectToAction("Index", "Home");
         }
---
+++ log-reg/Models/UsersViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace log_reg.Models
+{
+    public class UsersViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Contact { get; set; }
+
+    }
+}
