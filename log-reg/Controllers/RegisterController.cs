9cf473a73c339e56c753c3f51a629ad22d8f1488
shade-sicsr <shadedev25@gmail.com>
2023-10-10 21:45:45 +0530

    weak_credentials fix

--- log-reg/Controllers/RegisterController.cs
+++ log-reg/Controllers/RegisterController.cs
@@ -1,5 +1,6 @@
 ﻿using log_reg.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace log_reg.Controllers
 {
@@ -21,14 +22,48 @@ namespace log_reg.Controllers
             }
             return existing;
         }
+        public static bool CheckIfPasswordValid(string password, string user_name)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[A-Z]"))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[a-z]"))
+            {
+                return false;
+            }
+
+            if (!Regex.IsMatch(password, "[0-9]"))
+            {
+                return false;
+            }
+
+            if (Regex.IsMatch(password, user_name, RegexOptions.IgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
         public IActionResult RegisterUser(Users user_model)
         {
             if (ModelState.IsValid)
             {
-                if ((user_model.Password).Length >= 5)
+                string email = user_model.Email;
+                if (!CheckPreExisting(email))
                 {
-                    string email = user_model.Email;
-                    if (!CheckPreExisting(email))
+                    if (CheckIfPasswordValid(user_model.Password, user_model.Username))
                     {
                         _context.UsersObjects.Add(user_model);
                         _context.SaveChanges();
@@ -36,13 +71,13 @@ namespace log_reg.Controllers
                     }
                     else
                     {
-                        TempData["PreExistingUserError"] = "An account has been already registered to this email.";
+                        TempData["PasswordError"] = "The password should be at least 8 characters, contain one uppercase letter, one number and should not contain your name .";
                         return View("~/Views/Home/Register.cshtml");
                     }
                 }
                 else
                 {
-                    TempData["PasswordLimitError"] = "An account has been already registered to this email.";
+                    TempData["PreExistingUserError"] = "An account has been already registered to this email.";
                     return View("~/Views/Home/Register.cshtml");
                 }
             }
